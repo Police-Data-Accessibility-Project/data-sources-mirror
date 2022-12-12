8e1a83e5fdd9b61855a1129279eb2a397d08d748
Jacob Sanders <j.q.sanders@gmail.com>
2022-12-12 17:55:15 -0500

    create stub for script

---
+++ mirror.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# python imports
+import csv
+import os
+
+
+
+
+if __name__ == "__main__":
+    counties_filename = "counties.csv"
+    agencies_filename = "agencies.csv"
+    sources_filename = "sources.csv"
