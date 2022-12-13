50e0040d7678d111678c80456d81898b3f02cc8e
Jacob Sanders <j.q.sanders@gmail.com>
2022-12-13 12:02:41 -0500

    build objects for writing to csv

--- mirror.py
+++ mirror.py
@@ -3,6 +3,7 @@
 # python imports
 from collections import namedtuple
 import csv
+import datetime
 import os
 
 # third-party imports
@@ -19,7 +20,244 @@ def get_table_data(name):
         name
     ).all(fields=fieldnames, formula=os.environ["FORMULA"])
 
-    return Sheet(fieldnames, data)
+    rows = process_data(name, rows)
+
+    return Sheet(fieldnames, rows)
+
+
+def process_data(table_name, data):
+    # break out the nested stuff before passing this along for use;
+    # nothing should have to care about the original
+    # structure beyond this point
+    rows = (d["fields"] for d in data.rows)
+
+    # this will get a little weird: the Airtable API doesn't include
+    # fields with records that have falsy values;
+    # each record might have different fields missing than others,
+    # and there's no way in advance to determine which ones.
+    # so get ready for lots of sniffing and setting of nulls
+    if table_name == "Agencies":
+        processed = process_agencies(rows)
+    elif table_name == "Data Sources":
+        processed = process_sources(rows)
+
+    return Sheet(data.headers, processed)
+
+
+def process_agencies(data):
+    processed = []
+    for agency in data:
+        county_fips = None
+        if agency.get("county_fips", None):
+            county_fips = agency["county_fips"]
+
+        county_name = None
+        if agency.get("county_name", None):
+            county_name = agency["county_name"]
+
+        defunct_year = None
+        if agency.get("defunct_year", None):
+            defunct_year = agency["defunct_year"]
+        
+        homepage_url = None
+        if agency.get("homepage_url", None):
+            homepage_url = agency["homepage_url"]
+
+        jurisdiction_type = None
+        if agency.get("jurisdiction_type", None):
+            jurisdiction_type = agency["jurisdiction_type"]
+
+        lat = None
+        if agency.get("lat", None):
+            lat = agency["lat"]
+
+        lng = None
+        if agency.get("lng", None):
+            lng = agency["lng"]
+
+        municipality = None
+        if agency.get("municipality", None):
+            municipality = agency["municipality"]
+
+        name = None
+        if agency.get("name", None):
+            name = agency["name"]
+
+        state_iso = None
+        if agency.get("state_iso", None):
+            state_iso = agency["state_iso"]
+
+        submitted_name = None
+        if agency.get("submitted_name", None):
+            submitted_name = agency["submitted_name"]
+
+        row = {
+            "name": name,
+            "submitted_name": submitted_name,
+            "homepage_url": homepage_url,
+            "jurisdiction_type": jurisdiction_type,
+            "state_iso": state_iso,
+            "municipality": municipality,
+            "county_fips": county_fips,
+            "county_name": county_name,
+            "lat": lat,
+            "lng": lng,
+            "defunct_year": defunct_year,
+            "airtable_uid": agency["airtable_uid"]
+        }
+
+        processed.append(row)
+
+    return processed
+
+
+def process_sources(data):
+    processed = []
+    for source in data:
+        agency_linked = None
+        if source.get("agency_described_linked_uid", None):
+            # TODO: handle cases where there is more than one item in the list
+            agency_linked = source["agency_described_linked_uid"]
+
+        start_raw = source.get("coverage_start", None)
+        start = datetime.datetime.strptime(start_raw, "%Y-%m-%d") if start_raw else None
+
+        end_raw = source.get("coverage_end", None)
+        end = datetime.datetime.strptime(end_raw, "%Y-%m-%d") if end_raw else None
+
+        access_type = None
+        if source.get("access_type", None):
+            access_type = source["access_type"]
+
+        aggregated = None
+        if source.get("aggregation_type", None):
+            if source["aggregation_type"] == "Individual records" or source["aggregation_type"] == "no":
+                aggregated = False
+            elif source["aggregation_type"] == "":
+                aggregated = None
+            else:
+                aggregated = True
+
+        description = None
+        if source.get("description", None):
+            description = source["description"]
+
+        download_option = None
+        if source.get("record_download_option_provided", None):
+            download_option = source["record_download_option_provided"]
+
+        num_records = None
+        if source.get("number_of_records_available", None):
+            num_records = int(source["number_of_records_available"])
+
+        originated = None
+        if source.get("agency_originated", None):
+            if source["agency_originated"] == "yes":
+                originated = True
+            elif source["agency_originated"] == "no":
+                originated = False
+        
+        originating_entity = None
+        if source.get("originating_entity", None):
+            # why does this have line endings in the string? c/mon people
+            originating_entity = source["originating_entity"].strip()
+
+        portal_type = None
+        if source.get("data_portal_type", None):
+            portal_type = source["data_portal_type"]
+
+        record_format = []
+        if source.get("record_format", None):
+            record_format = source["record_format"]
+
+        record_type = None
+        if source.get("record_type", None):
+            record_type = source["record_type"]
+
+        restrictions = None
+        if source.get("access_restrictions", None):
+            restrictions = source["access_restrictions"]
+
+        restrictions_notes = None
+        if source.get("access_restrictions_notes", None):
+            restrictions_notes = source["access_restrictions_notes"]
+
+        supplied = None
+        if source.get("agency_supplied", None):
+            if source["agency_supplied"] == "yes":
+                supplied = True 
+            elif source["agency_supplied"] == "no":
+                supplied = False
+
+        retention_schedule = None
+        if source.get("retention_schedule", None):
+            retention_schedule = source["retention_schedule"]
+
+        size = None
+        if source.get("size", None):
+            size = source["size"]
+
+        sort_method = None
+        if source.get("sort_method", None):
+            size = source["sort_method"]
+
+        source_last_updated = None
+        if source.get("source_last_updated", None):
+            source_last_updated = datetime.datetime.strptime(source["source_last_updated"], "%Y-%m-%d")
+
+        source_url = None
+        if source.get("source_url", None):
+            source_url = source["source_url"]
+
+        submitted_name = None
+        if source.get("submitted_name", None):
+            submitted_name = source["submitted_name"]
+
+        supplying_entity = None
+        if source.get("supplying_entity", None):
+            supplying_entity = [e.strip for e in source["supplying_entity"]]
+
+        update_frequency = None
+        if source.get("update_frequency", None):
+            update_frequency = source["update_frequency"]
+
+        update_method = None
+        if source.get("update_method", None):
+            update_method = source["update_method"]
+
+        row = {
+            "access_restrictions": restrictions,
+            "access_restrictions_notes": restrictions_notes,
+            "access_type": access_type,
+            "agency_described": agency_linked,
+            "agency_originated": originated,
+            "agency_supplied": supplied,
+            "aggregated": aggregated,
+            "airtable_uid": source["airtable_uid"],
+            "coverage_start": start,
+            "coverage_end": end,
+            "data_portal_type": portal_type,
+            "description": description,
+            "name": source["name"],
+            "number_of_records_available": num_records,
+            "originating_entity": originating_entity,
+            "record_download_option_provided": download_option,
+            "record_format": record_format,
+            "record_type": record_type,
+            "retention_schedule": retention_schedule,
+            "size": size,
+            "sort_method": sort_method,
+            "source_last_updated": source_last_updated,
+            "source_url": source_url,
+            "submitted_name": submitted_name,
+            "supplying_entity": supplying_entity,
+            "update_frequency": update_frequency,
+            "update_method": update_method,
+        }
+
+        processed.append(row)
+
+    return processed
 
 
 def get_fieldnames(name):
@@ -58,6 +296,7 @@ def source_fieldnames():
         "supplying_entity",
         "agency_originated",
         "originating_entity",
+        "aggregation_type",
         "coverage_start",
         "coverage_end",
         "source_last_updated",
@@ -79,7 +318,6 @@ def source_fieldnames():
 
 def write_csv(data_package, location):
     pass
-
 
 
 
